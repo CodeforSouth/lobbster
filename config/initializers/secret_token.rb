e76ecec0cab5f72533530ebcd66b160b1105d95f
Julie <jkramer@mdlive.com>
2017-11-07 09:00:58 -0500

    initial commit with pg and rspec

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lobbster::Application.config.secret_token = '1169f18ce69afe8814198ef033cc11048d3b4d4f52ee600e791eb3be62fa25235df4aa1bd24abd9498f4d0d41aeaf5909e681c41a5ee954ad1442f36e7f37427'
