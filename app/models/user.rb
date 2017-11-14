3dd4a76a7ec91f1fa8aca77787dd415697a56eee
Ed Gonzalez <ergonzalezpmp@gmail.com>
2017-11-13 20:17:33 -0500

    Added user tests and some validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ApplicationRecord
+
+  VALID_EMAIL_REGEX = /\A[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}\z/i
+
+  validates_format_of :username, with: VALID_EMAIL_REGEX, message: 'Please provide a valid email.'
+  validates_uniqueness_of :username, message: 'Email is already registered'
+
+  validates_length_of :password, minimum: 6, maximum: 16, :message => "Password Must Be Longer Than 6 Characters."
+  validates_presence_of :password_confirmation, :message => "Password Confirmation Must Match Given Password."
+
+
+
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,58 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+
+RSpec.describe  User do
+  it "it creates a User on .new" do
+    assert_instance_of User, (User.new)
+  end
+
+  it "User has a .email method" do
+    assert_respond_to (User.new), :email
+  end
+
+  it "User has a .user_type method" do
+    assert_respond_to (User.new), :user_type
+  end
+
+  it "User has a .password method" do
+    assert_respond_to (User.new), :password
+  end
+
+  it "User has a .token method" do
+    assert_respond_to (User.new), :token
+  end
+
+  it "User has a .first_name method" do
+    assert_respond_to (User.new), :first_name
+  end
+
+  it "User has a .last_name method" do
+    assert_respond_to (User.new), :last_name
+  end
+
+  it "User has a .middle_initial method" do
+    assert_respond_to (User.new), :middle_initial
+  end
+
+  it "validates :email presence" do
+    new_user = User.new( password: "password", password_confirmation: "password")
+   refute new_user.save
+  end
+
+  it "validates :password presence" do
+   new_user = User.new( email: "ed@ed.com", password_confirmation: "password")
+   refute new_user.save
+  end
+
+  it "User rejects identical emails" do
+     valid_user = User.create(email: "ed@ed.com", password: "password", password_confirmation: "password")
+     new_user = User.new(email: "ed@ed.com", password: "password", password_confirmation: "password")
+     refute new_user.save
+  end
+
+  it "confirms password input" do
+    new_user = User.new(email: "ed@ed.com", password: "password", password_confirmation: "notpassword")
+    refute new_user.save
+  end
 end
