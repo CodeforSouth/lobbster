5e047c903b8c47da432992c98e8b959d14b56c5d
Julie <jkramer@mdlive.com>
2017-12-20 15:20:34 -0500

    Add request spec for POST issues create

--- spec/requests/issues_spec.rb
+++ spec/requests/issues_spec.rb
@@ -70,4 +70,43 @@ RSpec.describe "Issues", type: :request do
       end
     end
   end
+
+  describe "POST /issues/" do
+    let(:user) {create(:user)}
+    let(:valid_issue_params) {{
+      user_id: user.id,
+      principal: "Principal",
+      address: '123 Street',
+      city: 'Miami',
+      description: 'Approval of plans before Board of Architects',
+      ein_number: 123456789,
+      non_profit: false,
+      phone: '305-305-3050',
+      state: 'FL',
+      zip: 33317 }}
+
+    context "creates an issue" do
+
+      it "responds with a :created status" do
+        post issues_path, params: { issue: valid_issue_params }
+
+        expect(response).to have_http_status(201)
+      end
+
+      it "responds with created issue" do
+        post issues_path, params: { issue: valid_issue_params }
+
+        response_body = JSON.parse(response.body)
+        expect(response_body["principal"]).to eq "Principal"
+        expect(response_body["description"]).to eq 'Approval of plans before Board of Architects'
+        expect(response_body["address"]).to eq '123 Street'
+        expect(response_body["city"]).to eq 'Miami'
+        expect(response_body["ein_number"]).to eq 123456789
+        expect(response_body["non_profit"]).to eq false
+        expect(response_body["phone"]).to eq '305-305-3050'
+        expect(response_body["state"]).to eq 'FL'
+        expect(response_body["zip"]).to eq 33317
+      end
+    end
+  end
 end
