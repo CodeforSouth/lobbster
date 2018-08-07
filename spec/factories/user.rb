5a9869559e18d43beb8293b401fa523b464d81a6
Ryan Prince <rnprince7@gmail.com>
2018-08-06 20:38:32 -0400

    Remove Ruby files, initialize client with create-react-app.

--- spec/factories/user.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-FactoryBot.define do
-  factory :user do
-  end
-end
--- spec/support/request_spec_helper.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-module RequestSpecHelper
-  # Parse JSON response to ruby hash
-  def json
-    JSON.parse(response.body)
-  end
-end
