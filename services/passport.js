de42575530c6c0beb7821ff892763e1b4ee309f0
Ryan Prince <ryan@selvans.com>
2018-09-18 23:35:50 -0400

    Make excluding passwordHash from database responses the default action.

--- services/passport.js
+++ services/passport.js
@@ -12,7 +12,7 @@ passport.serializeUser((user, done) => {
 passport.deserializeUser((id, done) => {
   User.findById(id, (err, user) => {
     done(err, user);
-  }).select('-passwordHash');
+  });
 });
 
 passport.use(new LocalStrategy(
@@ -21,10 +21,11 @@ passport.use(new LocalStrategy(
     passwordField: 'password'
   },
   async (emailAddress, password, done) => {
-    const existingUser = await User.findOne({ emailAddress });
+    const existingUser = await User.findOne({ emailAddress }).select('+passwordHash');
     if (existingUser) {
       const correctPassword = await match(password, existingUser.passwordHash);
       if (correctPassword) {
+        existingUser.passwordHash = '';
         return done(null, existingUser);
       }
       return done(null, false, { message: 'Invalid password.' });
