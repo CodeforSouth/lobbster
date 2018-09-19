b7bc98a09de929170e2720eff5b9da8c5d59d799
Ryan Prince <ryan@selvans.com>
2018-09-19 17:11:25 -0400

    Secure the admin api to be admin-only.

--- routes/authorization.js
+++ routes/authorization.js
@@ -46,4 +46,15 @@ module.exports = (app) => {
     req.logout();
     res.status(200).send();
   });
+
+  // Middleware to make the admin API admin-only.
+  app.use('/api/admin', (req, res, next) => {
+    if (!req.user) {
+      res.status(401).send();
+    }
+    if (!req.user.isAdmin) {
+      res.status(403).send();
+    }
+    next();
+  });
 };
