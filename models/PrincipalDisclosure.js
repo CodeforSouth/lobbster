d67238c7f13edfb5e84b965e97f88f9ccb9af2bb
Ryan Prince <ryan@selvans.com>
2018-09-29 17:34:44 -0400

    Make all field required.

--- models/PrincipalDisclosure.js
+++ models/PrincipalDisclosure.js
@@ -4,20 +4,20 @@ const { Schema } = mongoose;
 const { ObjectId } = Schema.Types;
 
 const Expense = new Schema({
-  name: String,
-  amount: Number
+  name: { type: String, required: true },
+  amount: { type: Number, required: true }
 });
 
 const Issue = new Schema({
-  name: String,
-  expenses: [Expense]
+  name: { type: String, required: true },
+  expenses: { type: [Expense], required: true }
 });
 
 const PrincipalDisclosure = new Schema({
-  lobbyistId: ObjectId,
-  principalName: String,
-  reportingYear: Number,
-  issues: [Issue]
+  lobbyistId: { type: ObjectId, required: true },
+  principalName: { type: String, required: true },
+  reportingYear: { type: Number, required: true },
+  issues: { type: [Issue], required: true }
 });
 PrincipalDisclosure.index({ lobbyistId: 1, principalName: 1, reportingYear: 1 }, { unique: true });
 
