a9c122f69acc5181fbf2c5e08df94b9999a61b6e
naaltunian <naaltunian@icloud.com>
2018-10-03 13:27:52 -0400

    set up dev keys

--- config/prod-keys.js
+++ config/prod-keys.js
@@ -1,4 +1,11 @@
+// module.exports = {
+//   mongoURI: process.env.MONGO_URI
+//   expressSessionSecret: process.env.EXPRESS_SESSION_SECRET
+// };
+
+import MONGO_URI from "./dev-keys.js";
+
 module.exports = {
-  mongoURI: process.env.MONGO_URI,
-  expressSessionSecret: process.env.EXPRESS_SESSION_SECRET
+  mongoURI: MONGO_URI,
+  expressSessionSecret: "devTest"
 };
