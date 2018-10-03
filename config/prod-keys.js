e67d37f8b15162df42f4ecb92821289eda3587df
naaltunian <naaltunian@icloud.com>
2018-10-03 15:50:53 -0400

    refactor dev keys

--- config/prod-keys.js
+++ config/prod-keys.js
@@ -1,11 +1,4 @@
-// module.exports = {
-//   mongoURI: process.env.MONGO_URI
-//   expressSessionSecret: process.env.EXPRESS_SESSION_SECRET
-// };
-
-import MONGO_URI from "./dev-keys.js";
-
 module.exports = {
-  mongoURI: MONGO_URI,
-  expressSessionSecret: "devTest"
+  mongoURI: process.env.MONGO_URI
+  expressSessionSecret: process.env.EXPRESS_SESSION_SECRET
 };
