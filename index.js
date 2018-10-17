c7829a0df84b389e99061759ab0ec135b9286949
Ryan Prince <ryan@selvans.com>
2018-10-17 11:52:25 -0400

    Add routing for production client's requests.

--- index.js
+++ index.js
@@ -38,6 +38,14 @@ require('./routes/principalDisclosure')(app);
 require('./routes/paymentDocumentation')(app);
 require('./routes/billingRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}.`);
