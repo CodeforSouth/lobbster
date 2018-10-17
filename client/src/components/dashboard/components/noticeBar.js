170a780a2b83a9434be407c275df3499015dfcdc
Ryan Prince <ryan@selvans.com>
2018-10-17 16:40:56 -0400

    Implement the key property for react.

--- client/src/components/dashboard/components/noticeBar.js
+++ client/src/components/dashboard/components/noticeBar.js
@@ -3,16 +3,17 @@ import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export function noticeBar(noticeList) {
+  const noticeKeys = noticeList.map((notice, i) => i);
   return (
-    <div columns>
-      { noticeList.map(notice => <div className="column is-6">{notice}</div>) }
+    <div>
+      { noticeList.map((notice, i) => <div className="column is-6" key={noticeKeys[i]}>{notice}</div>) }
     </div>
   );
 }
 
-export function verifyEmailNotice(key) {
+export function verifyEmailNotice() {
   return (
-    <div key={key} className="notification is-warning">
+    <div className="notification is-warning">
       <article className="media">
         <figure className="media-left">
           <span className="icon">
@@ -32,9 +33,9 @@ export function verifyEmailNotice(key) {
   );
 }
 
-export function paymentNotice(key) {
+export function paymentNotice() {
   return (
-    <div key={key} className="notification is-warning">
+    <div className="notification is-warning">
       <article className="media">
         <figure className="media-left">
           <span className="icon">
@@ -43,7 +44,9 @@ export function paymentNotice(key) {
         </figure>
         <div className="media-content">
           <p>
-            Annual lobbyist registration fee is due for registered lobbyists. If eligible, you can request a non-profit lobbying exemption.
+            Annual lobbyist registration fee is due for registered lobbyists. If
+            { ' ' }
+            eligible, you can request a non-profit lobbying exemption.
           </p>
           <p>
             <Link to="/payments">My Payments</Link>
