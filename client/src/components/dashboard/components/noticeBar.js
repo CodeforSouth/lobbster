b5d007d0bfd737b20c880b3dd11ad2fcee6feb28
Ryan Prince <ryan@selvans.com>
2018-10-17 12:18:41 -0400

    Create a new notice for when users need to pay their lobbyist fee.

--- client/src/components/dashboard/components/noticeBar.js
+++ client/src/components/dashboard/components/noticeBar.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export function noticeBar(noticeList) {
   return (
-    <div className="level">
-      { noticeList }
+    <div columns>
+      { noticeList.map(notice => <div className="column is-6">{notice}</div>) }
     </div>
   );
 }
@@ -24,6 +25,28 @@ export function verifyEmailNotice(key) {
           </p>
           <p>
             <a href="/">Resend Link</a>
+          </p>
+        </div>
+      </article>
+    </div>
+  );
+}
+
+export function paymentNotice(key) {
+  return (
+    <div key={key} className="notification is-warning">
+      <article className="media">
+        <figure className="media-left">
+          <span className="icon">
+            <FontAwesomeIcon icon="exclamation-triangle" />
+          </span>
+        </figure>
+        <div className="media-content">
+          <p>
+            Annual lobbyist registration fee is due for registered lobbyists. If eligible, you can request a non-profit lobbying exemption.
+          </p>
+          <p>
+            <Link to="/payments">My Payments</Link>
           </p>
         </div>
       </article>
