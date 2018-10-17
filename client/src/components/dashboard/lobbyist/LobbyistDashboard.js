4baa26d27f1a4e0dfac7f9d01a59d9f857d7525f
Ryan Prince <ryan@selvans.com>
2018-10-17 12:30:14 -0400

    Show payment needed notice when appropriate.

--- client/src/components/dashboard/lobbyist/LobbyistDashboard.js
+++ client/src/components/dashboard/lobbyist/LobbyistDashboard.js
@@ -1,31 +1,66 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-import { noticeBar, verifyEmailNotice } from '../components/noticeBar';
+import { noticeBar, verifyEmailNotice, paymentNotice } from '../components/noticeBar';
 import DisclosuresOverview from '../components/DisclosuresOverview';
-import discloseNewPrincipalButton from '../components/discloseNewPrincipalButton';
+import { fetchPaymentsDocumentation } from '../../../requests/paymentDocumentation';
 
-const LobbyistDashboard = ({ user }) => (
-  <div className="hero-body has-background-white">
-    <div className="container has-text-left">
-      <div className="column is-10 is-offset-1">
-        <div className="container">
-          { !user.emailVerified && noticeBar([verifyEmailNotice(1)]) }
-        </div>
-        <div className="container">
-          <DisclosuresOverview
-            lobbyistId={user.id}
-            includeLobbyistName={false}
-            linkToDisclosure={true}
-          />
-        </div>
-        <div className="container">
-          { discloseNewPrincipalButton() }
+function paymentNeedsAttention(paymentsDocumentation) {
+  const unsettledStatus = ['action_needed', 'exemption_rejected'];
+  for (let i = 0; i < paymentsDocumentation.length; i += 1) {
+    const documentation = paymentsDocumentation[i];
+    if (unsettledStatus.indexOf(documentation.status) !== -1) {
+      return true;
+    }
+  }
+  return false;
+}
+
+class LobbyistDashboard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      paymentsDocumentation: []
+    };
+  }
+
+  async componentDidMount() {
+    const { user } = this.props;
+    const paymentsDocumentation = await fetchPaymentsDocumentation(user._id) || [];
+    this.setState({ paymentsDocumentation });
+  }
+
+  render() {
+    const { paymentsDocumentation } = this.state;
+    const { user } = this.props;
+    const notices = [];
+    if (!user.emailVerified) {
+      notices.push(verifyEmailNotice(1));
+    }
+    if (paymentNeedsAttention(paymentsDocumentation)) {
+      notices.push(paymentNotice(2));
+    }
+    return (
+      <div className="hero-body has-background-white">
+        <div className="container has-text-left">
+          <div className="column is-10 is-offset-1">
+            <div className="container">
+              { !user.emailVerified && noticeBar(notices) }
+            </div>
+            <div className="container">
+              <DisclosuresOverview
+                lobbyistId={user.id}
+                includeLobbyistName={false}
+                linkToDisclosure
+                showAddButton
+              />
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  </div>
-);
+    );
+  }
+}
 
 LobbyistDashboard.propTypes = {
   user: PropTypes.object.isRequired
